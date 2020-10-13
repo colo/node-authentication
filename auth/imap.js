4b5c0a23dd10c9e4abf425431779e1d34d917199
Luciano Bueno <glamero@gmail.com>
2020-10-13 17:21:58 -0300

    Fixed ImapAuth to work with node-imap v0.8.x

--- auth/imap.js
+++ auth/imap.js
@@ -1,38 +1,48 @@
 var Auth = require('./auth');
 
-var ImapConnection = require('imap').ImapConnection;
+var Imap = require('imap');
 
 module.exports =  new Class({
   Extends: Auth,
-  
+
   options: {
 		host: 'localhost',
 		port: 143,
-		secure: false
+    autotls: 'always',
+		tls: false,
+    // tlsOptions: {
+    //   maxVersion: 'TLSv1.2',
+    //   minVersion: 'TLSv1.2',
+    // },
+    // debug: function(debug){
+    //   console.log(debug)
+    // }
   },
-  
+
   initialize: function(options){
 		this.parent(options);
   },
   authenticate: function (username, password, fn) {
-	
-		var imap = new ImapConnection(Object.merge({
-			username: username,
+
+		var imap = new Imap(Object.merge({
+			user: username,
 			password: password
 		}, this.options));
-		
-		imap.connect(function(err){
 
-			if(err){
-				return fn(err, null);
-			}
-			
-			imap.logout(function(){
-				return fn(null, username);
-			});//logout
-			
-		});//connect
-	
-	
+    imap.once('ready', function() {
+      imap.end()
+      return fn(null, username);
+    })
+    imap.once('error', function(err) {
+      return fn(err, null)
+    });
+
+    imap.once('end', function() {
+      // console.log('Connection ended');
+    });
+
+    imap.connect();//connect
+
+
   },
 });
