aa1a429d5cc7e15369909834b7e72ba70e2188de
Luciano Bueno <glamero@gmail.com>
2020-10-13 20:40:32 -0300

    Changed to async funcs (callback)

--- store/memory.js
+++ store/memory.js
@@ -2,12 +2,12 @@ var Store = require('./store');
 
 module.exports =  new Class({
   Extends: Store,
-  
+
   users: [],
-  
+
   options: {
   },
-  
+
   initialize: function(users, callback){
 		if(callback){
 			this.load(users, callback);
@@ -33,7 +33,7 @@ module.exports =  new Class({
 			if (user['id'] == id) {
 				notFound = false;
 				done(null, user);
-			} 
+			}
 		});
 
 		if(notFound === true)
@@ -43,41 +43,41 @@ module.exports =  new Class({
   /**
    * needed for node-express-authentication->passport intregration
    * */
-  findByUserName: function(username){
+  findByUserName: function(username, callback){
 		//var user = new Error('user not found');
 		var user = false;
 		//user.username = username;
-		
+
 		this.users.each(function(u){
 			if (u.username == username) {
 				user = u;
 			}
 		});
 
-		return user;
+		callback( user );
   },
-  
-  findByID: function(id){
+
+  findByID: function(id, callback){
 		var user = new Error('user not found');
 		user.id = id;
-		
+
 		this.users.each(function(u){
 			if (u.id == id) {
 				user = u;
 			}
 		});
 
-		return user;
+		callback( user );
   },
-  findByToken: function(token){
+  findByToken: function(token, callback){
 		var user = new Error('user not found');
 		user.token = token;
-		
+
 		this.users.each(function(u){
 			if (u.token) {
 				/**
 				 * may add other tokken formats in the future, now just uuid
-				 * 
+				 *
 				 * */
 				if(u.token.uuid == token){
 					user = u;
@@ -85,16 +85,16 @@ module.exports =  new Class({
 			}
 		});
 
-		return user;
+		callback( user );
   },
   //load: function(users){
 		//users.each(function(u){
 			//this.add(u);
 		//}.bind(this));
 	//},
-	
+
 	add: function(user, callback){
-		
+
 		if(user.id && user.username){
 			this.users.push(user);
 			this.parent(user, callback);
@@ -114,18 +114,18 @@ module.exports =  new Class({
 					 throw new Error('user found');
 				}
 			}.bind(this));
-			
+
 			var err = new Error('user not found');
 			err.user = user;
 			this.parent(err, callback);
 		}
 		catch(e){
 			//console.log(e);
-			
+
 			this.parent(user, callback);
 		}
-		
-		
+
+
 	},
 	update: function(user, callback){
 		try{
@@ -136,19 +136,19 @@ module.exports =  new Class({
 					 throw new Error('user found');
 				}
 			}.bind(this));
-			
+
 			var err = new Error('user not found');
 			err.user = user;
-			
+
 			this.parent(err, callback);
 		}
 		catch(e){
 			//console.log(e);
-			
+
 			this.parent(user, callback);
 		}
-		
-		
+
+
 	},
 	save: function(callback){
 		callback(null, true);
--- store/store.js
+++ store/store.js
@@ -2,15 +2,15 @@ var moootools = require('mootools');
 
 module.exports =  new Class({
   Implements: [Options, Events],
-  
+
   ON_ADD: 'onAdd',
   ON_REMOVE: 'onRemove',
   ON_UPDATE: 'onUpdate',
-  
+
   options: {
   },
-  
-  
+
+
   initialize: function(options){
 		this.setOptions(options);
   },
@@ -29,31 +29,34 @@ module.exports =  new Class({
   /**
    * needed for node-express-authentication->passport intregration
    * */
-  findByUserName: function(username){
-		return null;
+  findByUserName: function(username, callback){
+		callback( null );
   },
-  
-  findByID: function(id){
-		return null;
+
+  findByID: function(id, callback){
+		callback( null );
   },
-  
+  findByToken: function(token, callback){
+		callback( null );
+  },
+
   load: function(users, callback){
 		var failed = [];
-		
-		
+
+
 		users.each(function(u){
 			this.add(u, function(err, user){
 				failed.push([err, user]);
 			});
 		}.bind(this));
-		
+
 		callback(failed);
 	},
 	list: function(callback){
 		callback(this.users);
 	},
 	add: function(user, callback){
-		
+
 		if(user instanceof Error){
 			callback(user.message, user.user);
 		}
@@ -61,7 +64,7 @@ module.exports =  new Class({
 			this.fireEvent(this.ON_ADD, user);
 			callback(null, user);
 		}
-		
+
 	},
 	remove: function(user, callback){
 		if(user instanceof Error){
@@ -71,10 +74,10 @@ module.exports =  new Class({
 			this.fireEvent(this.ON_REMOVE, user);
 			callback(null, user);
 		}
-		
+
 	},
 	update: function(user, callback){
-		
+
 		if(user instanceof Error){
 			callback(user.message, user.user);
 		}
@@ -82,35 +85,35 @@ module.exports =  new Class({
 			this.fireEvent(this.ON_UPDATE, user);
 			callback(null, user);
 		}
-		
+
 	},
 	//removeByUserName: function(username, callback){
 		//var user = this.findByUserName(username);
 		//if(user != null)
 			//user = this.remove(user);
-			
+
 		//callback(null, user);
 	//},
 	//removeByID: function(id, callback){
 		//var user = this.findByID(id);
 		//if(user != null)
 			//user = this.remove(user);
-			
+
 		//callback(null, user);
 	//},
 	//updateByUserName: function(username, callback){
 		//var user = this.findByUserName(username);
 		//if(user != null)
 			//user = this.update(user);
-			
+
 		//callback(null, user);
 	//},
 	//updateByID: function(id, callback){
 		//var user = this.findByID(id);
-		
+
 		//if(user != null)
 			//user = this.update(user);
-			
+
 		//callback(null, user);
 	//},
 	save: function(callback){
